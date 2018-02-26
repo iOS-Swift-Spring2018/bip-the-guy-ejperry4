525e35123ff4f9351e8b8e7cc4dfa866a5dfbf97
Ej Perry <ejperry@ejs-macbook-pro.bc.edu>
2018-02-25 20:41:09 -0500

    Big the Guy App

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,19 +7,100 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    // MARK: Properties
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBOutlet weak var image: UIImageView!
+    
+    var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
+    
+    
+    // MARK: Functions
+    func animateImage (){
+        let bounds = self.image.bounds
+        let shrinkValue = CGFloat(60)
+        
+        // Shrink Image By 60
+        self.image.bounds = CGRect(x: self.image.bounds.origin.x + shrinkValue, y: self.image.bounds.origin.y + shrinkValue, width: self.image.bounds.size.width - shrinkValue, height: self.image.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.image.bounds = bounds}, completion: nil)
+        
+        
     }
-
-
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer) {
+        
+        if let sound = NSDataAsset(name: soundName) {
+            //Check if sound.data is a sound file
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+                
+                
+                
+            } catch {
+                print("ERROR: Sound in sound\(soundName) could not be played as audio" )
+            }
+        }else {
+            // Error File Didn't Load
+            print("Error file sound\(soundName) didn't load" )
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        image.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title:"Ok", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    // MARK: Actions
+    @IBAction func libraryPressed(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+    }
+    
+    @IBAction func cameraPressed(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            imagePicker.sourceType = .camera
+            
+            imagePicker.delegate = self
+            
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            showAlert(title: "Camera Not Avalible", message: "Found No Camera")
+        }
+     
+        
+    }
+    @IBAction func imageTapped(_ sender: Any) {
+        animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
+    }
+    
+    
 }
 
